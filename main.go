298c0250432aa72dfb9b27edf196c08361af66a3
mahdi-laptop <imani.mahdi@gmail.com>
2022-06-18 01:00:59 +0430

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	logger := log.New(os.Stderr, "", 0)
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		logger.Println("Someone call echo page")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello",
+			"version": "1",
+		})
+	})
+
+	r.Run(":3000")
+}
